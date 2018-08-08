516ed3e0decfdf242c196f2882b1e69915811638
elsitlte <1570056132@qq.com>
2018-08-08 17:34:19 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# pxt-superdriver
-麦极创客电机驱动库
+
+# Magibit Library for Micro:bit
+版本号: v0.1.2
+
+该扩展包为麦极创客专门为Micro:bit开发的语句库。推荐配合麦极创客的Magishield扩展板、Magibit主板和Magibit传感器套装进行使用，以便获得最好的体验。同时该包也支持市面上绝大多数的Micro:bit传感器模块和执行器，用户自行进行pin口连接即可。
+
+## 1. 当前版本支持的模块
+
+- 马达
+- 步进电机
+- 伺服电机
+
+## 2. 使用说明
+- 点击高级->添加软件包（add package），粘贴Magibit库的地址，https://github.com/Imagimaker/pxt-magibit
+- 点击右侧“搜索”按钮，单击找到的程序包，稍等片刻后，即可成功添加Magibit的语句库。
+![install](install.gif)
+
+## 3. 备注
+
+- 舵机推荐使用Micro:bit官方的语句进行控制，引脚->伺服机构（pin->servo）。
+- 灯带推荐使用Neopixel进行控制，在Add Package中搜索Neopixel即可添加。
+- 语句库中的摇杆模块的语句，硬件上只支持搭配Magibit摇杆模块进行使用，不支持第三方摇杆模块。如需使用第三方摇杆模块，用户需自行读取pin口数据。
+
+## Supported targets
+
+* for PXT/microbit
---
+++ magi-super-driver.ts
@@ -0,0 +1,286 @@
+
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+/**
+ * Custom blocks
+ */
+/**
+ * Functions are mapped to blocks using various macros 
+ * in comments starting with % (e.g., //% block).
+ * The most important macro "block" specifies that a
+ * block should be generated for a **exported** function.
+ */
+//% color="#AAc044" icon="\uf29e"
+namespace magiSuperDriver {
+
+    //% block
+    export function helloWorld() {
+
+    }
+
+    const PCA9685_ADDRESS = 0x40
+    const MODE1 = 0x00
+    const MODE2 = 0x01
+    const SUBADR1 = 0x02
+    const SUBADR2 = 0x03
+    const SUBADR3 = 0x04
+    const PRESCALE = 0xFE
+    const LED0_ON_L = 0x06
+    const LED0_ON_H = 0x07
+    const LED0_OFF_L = 0x08
+    const LED0_OFF_H = 0x09
+    const ALL_LED_ON_L = 0xFA
+    const ALL_LED_ON_H = 0xFB
+    const ALL_LED_OFF_L = 0xFC
+    const ALL_LED_OFF_H = 0xFD
+
+    const STP_CHA_L = 2047
+    const STP_CHA_H = 4095
+
+    const STP_CHB_L = 1
+    const STP_CHB_H = 2047
+
+    const STP_CHC_L = 1023
+    const STP_CHC_H = 3071
+
+    const STP_CHD_L = 3071
+    const STP_CHD_H = 1023
+
+    // HT16K33 commands
+    const HT16K33_ADDRESS = 0x70
+    const HT16K33_BLINK_CMD = 0x80
+    const HT16K33_BLINK_DISPLAYON = 0x01
+    const HT16K33_BLINK_OFF = 0
+    const HT16K33_BLINK_2HZ = 1
+    const HT16K33_BLINK_1HZ = 2
+    const HT16K33_BLINK_HALFHZ = 3
+    const HT16K33_CMD_BRIGHTNESS = 0xE0
+
+    export enum Servos {
+        S1 = 0x01,
+        S2 = 0x02,
+        S3 = 0x03,
+        S4 = 0x04,
+        S5 = 0x05,
+        S6 = 0x06,
+        S7 = 0x07,
+        S8 = 0x08
+    }
+
+    export enum Motors {
+        MS1 = 0x1,
+        MS2 = 0x2,
+        MS3 = 0x3,
+        MS4 = 0x4
+    }
+
+    export enum Steppers {
+        Step_1 = 0x1,
+        Step_2 = 0x2
+    }
+
+    export enum Turns {
+        //% blockId="T1B4" block="1/4"
+        T1B4 = 90,
+        //% blockId="T1B2" block="1/2"
+        T1B2 = 180,
+        //% blockId="T1B0" block="1"
+        T1B0 = 360,
+        //% blockId="T2B0" block="2"
+        T2B0 = 720,
+        //% blockId="T3B0" block="3"
+        T3B0 = 1080,
+        //% blockId="T4B0" block="4"
+        T4B0 = 1440,
+        //% blockId="T5B0" block="5"
+        T5B0 = 1800
+    }
+    export enum Directions {
+        //% block="0"
+        backward=0,
+        //% block="1"
+        forward=1
+    }
+    let initialized = false
+    let initializedMatrix = false
+    let neoStrip: neopixel.Strip;
+    let matBuf = pins.createBuffer(17);
+    let distanceBuf = 0;
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2ccmd(addr: number, value: number) {
+        let buf = pins.createBuffer(1)
+        buf[0] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+    function i2cread(addr: number, reg: number) {
+        pins.i2cWriteNumber(addr, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(addr, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function initPCA9685(): void {
+        i2cwrite(PCA9685_ADDRESS, MODE1, 0x00)
+        setFreq(50);
+        for (let idx = 0; idx < 16; idx++) {
+            setPwm(idx, 0 ,0);
+        }
+        initialized = true
+    }
+
+    function setFreq(freq: number): void {
+        // Constrain the frequency
+        let prescaleval = 25000000;
+        prescaleval /= 4096;
+        prescaleval /= freq;
+        prescaleval -= 1;
+        let prescale = prescaleval; //Math.Floor(prescaleval + 0.5);
+        let oldmode = i2cread(PCA9685_ADDRESS, MODE1);
+        let newmode = (oldmode & 0x7F) | 0x10; // sleep
+        i2cwrite(PCA9685_ADDRESS, MODE1, newmode); // go to sleep
+        i2cwrite(PCA9685_ADDRESS, PRESCALE, prescale); // set the prescaler
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode);
+        control.waitMicros(5000);
+        i2cwrite(PCA9685_ADDRESS, MODE1, oldmode | 0xa1);
+    }
+
+    function setPwm(channel: number, on: number, off: number): void {
+        if (channel < 0 || channel > 15)
+            return;
+
+        let buf = pins.createBuffer(5);
+        buf[0] = LED0_ON_L + 4 * channel;
+        buf[1] = on & 0xff;
+        buf[2] = (on >> 8) & 0xff;
+        buf[3] = off & 0xff;
+        buf[4] = (off >> 8) & 0xff;
+        pins.i2cWriteBuffer(PCA9685_ADDRESS, buf);
+    }
+
+
+    function setStepper(index: number, dir: boolean): void {
+        if (index == 1) {
+            if (dir) {
+                setPwm(0, STP_CHA_L, STP_CHA_H);
+                setPwm(2, STP_CHB_L, STP_CHB_H);
+                setPwm(1, STP_CHC_L, STP_CHC_H);
+                setPwm(3, STP_CHD_L, STP_CHD_H);
+            } else {
+                setPwm(3, STP_CHA_L, STP_CHA_H);
+                setPwm(1, STP_CHB_L, STP_CHB_H);
+                setPwm(2, STP_CHC_L, STP_CHC_H);
+                setPwm(0, STP_CHD_L, STP_CHD_H);
+            }
+        } else {
+            if (dir) {
+                setPwm(4, STP_CHA_L, STP_CHA_H);
+                setPwm(6, STP_CHB_L, STP_CHB_H);
+                setPwm(5, STP_CHC_L, STP_CHC_H);
+                setPwm(7, STP_CHD_L, STP_CHD_H);
+            } else {
+                setPwm(7, STP_CHA_L, STP_CHA_H);
+                setPwm(5, STP_CHB_L, STP_CHB_H);
+                setPwm(6, STP_CHC_L, STP_CHC_H);
+                setPwm(4, STP_CHD_L, STP_CHD_H);
+            }
+        }
+    }
+
+    function stopMotor(index: number) {
+        setPwm((index - 1) * 2, 0, 0);
+        setPwm((index - 1) * 2 + 1, 0, 0);
+    }
+
+        /**
+         * Servo Execute
+         * @param index Servo Channel; eg: S1
+         * @param degree [0-180] degree of servo; eg: 0, 90, 180
+        */
+        //% blockId=robotbit_servo block="Servo|%index|degree %degree"
+        //% weight=100
+        //% degree.min=0 degree.max=180
+        //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+        export function Servo(index: Servos, degree: number): void {
+            if (!initialized) {
+                initPCA9685()
+            }
+            // 50hz: 20,000 us
+            let v_us = (degree * 1800 / 180 + 600) // 0.6 ~ 2.4
+            let value = v_us * 4096 / 20000
+            setPwm(index + 7, 0, value)
+        }
+        
+        //% blockId=robotbit_stepper_degree block="Stepper |%index|degree %degree"
+        //% weight=90
+        export function StepperDegree(index: Steppers, degree: number): void {
+            if (!initialized) {
+                initPCA9685()
+            }
+            setStepper(index, degree > 0);
+            degree = Math.abs(degree);
+            basic.pause(10240 * degree / 360);
+            MotorStopAll()
+        }
+
+
+        //% blockId=robotbit_stepper_turn block="Stepper |%index|turn %turn"
+        //% weight=90
+        export function StepperTurn(index: Steppers, turn: Turns): void {
+            let degree = turn;
+            StepperDegree(index, degree);
+        }
+
+        //% blockId=robotbit_motor_run block="Motor|%index|running with direction|%direction|and speed %speed"
+        //% weight=85
+        //% speed.min=-1024 speed.max=1024
+        //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+        export function MotorRun(index: Motors, direction: Directions, speed: number): void {
+            if (!initialized) {
+                initPCA9685()
+            }
+            speed = speed * 4; // map 255 to 4096
+            if (speed >= 4096) {
+                speed = 4095
+            }
+            if (speed <= -4096) {
+                speed = -4095
+            }
+            if (index > 4 || index <= 0)
+                return
+            let pp = (index - 1) * 2
+            let pn = (index - 1) * 2 + 1
+            if (direction > 0) {
+                setPwm(pp, 0, speed)
+                setPwm(pn, 0, 0)
+            } else {
+                setPwm(pp, 0, 0)
+                setPwm(pn, 0, -speed)
+            }
+        }
+
+
+        //% blockId=robotbit_stop block="Motor Stop|%index|"
+        //% weight=80
+        export function MotorStop(index: Motors): void {
+            MotorRun(index, 0);
+        }
+
+        //% blockId=robotbit_stop_all block="Motor Stop All"
+        //% weight=79
+        //% blockGap=50
+        export function MotorStopAll(): void {
+            for (let idx = 1; idx <= 4; idx++) {
+                stopMotor(idx);
+            }
+        }
+}
