9ceb5b192ddaef60e29014642085157556436c62
elsitlte <1570056132@qq.com>
2018-08-10 09:44:32 +0800

    forth

--- main.ts
+++ main.ts
@@ -14,7 +14,12 @@
  * block should be generated for a **exported** function.
  */
 //% color="#AAc044" icon="\uf29e"
-namespace magisuperdriver {
+namespace me {
+
+    //% block
+    export function helloWorld666() {
+
+    }
 
     const PCA9685_ADDRESS = 0x40
     const MODE1 = 0x00
@@ -66,15 +71,15 @@ namespace magisuperdriver {
     }
 
     export enum Motors {
-        MS1 = 0x1,
-        MS2 = 0x2,
-        MS3 = 0x3,
-        MS4 = 0x4
+        M1A = 0x1,
+        M1B = 0x2,
+        M2A = 0x3,
+        M2B = 0x4
     }
 
     export enum Steppers {
-        Step_1 = 0x1,
-        Step_2 = 0x2
+        M1 = 0x1,
+        M2 = 0x2
     }
 
     export enum Turns {
@@ -93,12 +98,7 @@ namespace magisuperdriver {
         //% blockId="T5B0" block="5"
         T5B0 = 1800
     }
-    export enum Directions {
-        //% block="0"
-        backward=0,
-        //% block="1"
-        forward=1
-    }
+
     let initialized = false
     let initializedMatrix = false
 
@@ -196,85 +196,279 @@ namespace magisuperdriver {
         setPwm((index - 1) * 2 + 1, 0, 0);
     }
 
-        /**
-         * Servo Execute
-         * @param index Servo Channel; eg: S1
-         * @param degree [0-180] degree of servo; eg: 0, 90, 180
-        */
-        //% blockId=robotbit_servo block="Servo|%index|degree %degree"
-        //% weight=100
-        //% degree.min=0 degree.max=180
-        //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-        export function Servo(index: Servos, degree: number): void {
-            if (!initialized) {
-                initPCA9685()
-            }
-            // 50hz: 20,000 us
-            let v_us = (degree * 1800 / 180 + 600) // 0.6 ~ 2.4
-            let value = v_us * 4096 / 20000
-            setPwm(index + 7, 0, value)
+    // function matrixInit() {
+    //     i2ccmd(HT16K33_ADDRESS, 0x21);// turn on oscillator
+    //     i2ccmd(HT16K33_ADDRESS, HT16K33_BLINK_CMD | HT16K33_BLINK_DISPLAYON | (0 << 1));
+    //     i2ccmd(HT16K33_ADDRESS, HT16K33_CMD_BRIGHTNESS | 0xF);
+    // }
+
+    // function matrixShow() {
+    //     matBuf[0] = 0x00;
+    //     pins.i2cWriteBuffer(HT16K33_ADDRESS, matBuf);
+    // }
+
+    /**
+     * Servo Execute
+     * @param index Servo Channel; eg: S1
+     * @param degree [0-180] degree of servo; eg: 0, 90, 180
+    */
+    //% blockId=robotbit_servo block="Servo|%index|degree %degree"
+    //% weight=100
+    //% degree.min=0 degree.max=180
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function Servo(index: Servos, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
         }
-        
-        //% blockId=robotbit_stepper_degree block="Stepper|%index|degree %degree"
-        //% weight=90
-        export function StepperDegree(index: Steppers, degree: number): void {
-            if (!initialized) {
-                initPCA9685()
-            }
-            setStepper(index, degree > 0);
-            degree = Math.abs(degree);
-            basic.pause(10240 * degree / 360);
-            MotorStopAll()
+        // 50hz: 20,000 us
+        let v_us = (degree * 1800 / 180 + 600) // 0.6 ~ 2.4
+        let value = v_us * 4096 / 20000
+        setPwm(index + 7, 0, value)
+    }
+
+    // /**
+    //  * Geek Servo
+    //  * @param index Servo Channel; eg: S1
+    //  * @param degree [-45-225] degree of servo; eg: -45, 90, 225
+    // */
+    // //% blockId=robotbit_gservo block="Geek Servo|%index|degree %degree"
+    // //% weight=99
+    // //% blockGap=50
+    // //% degree.min=-45 degree.max=225
+    // //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    // export function GeekServo(index: Servos, degree: number): void {
+    //     if (!initialized) {
+    //         initPCA9685()
+    //     }
+    //     // 50hz: 20,000 us
+    //     let v_us = ((degree -90) * 20 / 3 + 1500) // 0.6 ~ 2.4
+    //     let value = v_us * 4096 / 20000
+    //     setPwm(index + 7, 0, value)
+    // }
+    
+    //% blockId=robotbit_stepper_degree block="Stepper 28BYJ-48|%index|degree %degree"
+    //% weight=90
+    export function StepperDegree(index: Steppers, degree: number): void {
+        if (!initialized) {
+            initPCA9685()
         }
+        setStepper(index, degree > 0);
+        degree = Math.abs(degree);
+        basic.pause(10240 * degree / 360);
+        MotorStopAll()
+    }
 
 
-        //% blockId=robotbit_stepper_turn block="Stepper|%index|turn %turn"
-        //% weight=90
-        export function StepperTurn(index: Steppers, turn: Turns): void {
-            let degree = turn;
-            StepperDegree(index, degree);
-	}
-        //% blockId=robotbit_motor_run block="Motor|%index|running with direction|%direction|and speed %speed"
-        //% weight=85
-        //% speed.min=-1024 speed.max=1024
-        //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
-        export function MotorRun(index: Motors, direction: Directions, speed: number): void {
-            if (!initialized) {
-                initPCA9685()
-            }
-            speed = speed * 4; // map 1024 to 4096
-            if (speed >= 4096) {
-                speed = 4095
-            }
-            if (speed <= -4096) {
-                speed = -4095
-            }
-            if (index > 4 || index <= 0)
-                return
-            let pp = (index - 1) * 2
-            let pn = (index - 1) * 2 + 1
-            if (direction > 0) {
-                setPwm(pp, 0, speed)
-                setPwm(pn, 0, 0)
-            } else {
-                setPwm(pp, 0, 0)
-                setPwm(pn, 0, -speed)
-            }
+    //% blockId=robotbit_stepper_turn block="Stepper 28BYJ-48|%index|turn %turn"
+    //% weight=90
+    export function StepperTurn(index: Steppers, turn: Turns): void {
+        let degree = turn;
+        StepperDegree(index, degree);
+    }
+
+    // //% blockId=robotbit_stepper_dual block="Dual Stepper(Degree) |M1 %degree1| M2 %degree2"
+    // //% weight=89
+    // export function StepperDual(degree1: number, degree2: number): void {
+    //     if (!initialized) {
+    //         initPCA9685()
+    //     }
+    //     setStepper(1, degree1 > 0);
+    //     setStepper(2, degree2 > 0);
+    //     degree1 = Math.abs(degree1);
+    //     degree2 = Math.abs(degree2);
+    //     basic.pause(10240 * Math.min(degree1, degree2) / 360);
+    //     if (degree1 > degree2) {
+    //         stopMotor(3); stopMotor(4);
+    //         basic.pause(10240 * (degree1 - degree2) / 360);
+    //     } else {
+    //         stopMotor(1); stopMotor(2);
+    //         basic.pause(10240 * (degree2 - degree1) / 360);
+    //     }
+
+    //     MotorStopAll()
+    // }
+
+    // /**
+    //  * Stepper Car move forward
+    //  * @param distance Distance to move in cm; eg: 10, 20
+    //  * @param diameter diameter of wheel in mm; eg: 48
+    // */
+    // //% blockId=robotbit_stpcar_move block="Car Forward|Diameter(cm) %distance|Wheel Diameter(mm) %diameter"
+    // //% weight=88
+    // export function StpCarMove(distance: number, diameter: number): void {
+    //     if (!initialized) {
+    //         initPCA9685()
+    //     }
+    //     let delay = 10240 * 10 * distance / 3 / diameter; // use 3 instead of pi
+    //     setStepper(1, delay > 0);
+    //     setStepper(2, delay > 0);
+    //     delay = Math.abs(delay);
+    //     basic.pause(delay);
+    //     MotorStopAll()    
+    // }
+
+    // /**
+    //  * Stepper Car turn by degree
+    //  * @param turn Degree to turn; eg: 90, 180, 360
+    //  * @param diameter diameter of wheel in mm; eg: 48
+    //  * @param track track width of car; eg: 125
+    // */
+    // //% blockId=robotbit_stpcar_turn block="Car Turn|Degree %turn|Wheel Diameter(mm) %diameter|Track(mm) %track"
+    // //% weight=87
+    // //% blockGap=50
+    // export function StpCarTurn(turn: number, diameter: number, track: number): void {
+    //     if (!initialized) {
+    //         initPCA9685()
+    //     }
+    //     let delay = 10240 * turn * track / 360 / diameter;
+    //     setStepper(1, delay < 0);
+    //     setStepper(2, delay > 0);
+    //     delay = Math.abs(delay);
+    //     basic.pause(delay);
+    //     MotorStopAll()
+    // }
+
+    //% blockId=robotbit_motor_run block="Motor|%index|speed %speed"
+    //% weight=85
+    //% speed.min=-255 speed.max=255
+    //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    export function MotorRun(index: Motors, speed: number): void {
+        if (!initialized) {
+            initPCA9685()
         }
+        speed = speed * 16; // map 255 to 4096
+        if (speed >= 4096) {
+            speed = 4095
+        }
+        if (speed <= -4096) {
+            speed = -4095
+        }
+        if (index > 4 || index <= 0)
+            return
+        let pp = (index - 1) * 2
+        let pn = (index - 1) * 2 + 1
+        if (speed >= 0) {
+            setPwm(pp, 0, speed)
+            setPwm(pn, 0, 0)
+        } else {
+            setPwm(pp, 0, 0)
+            setPwm(pn, 0, -speed)
+        }
+    }
 
 
-        //% blockId=robotbit_stop block="Motor Stop|%index|"
-        //% weight=80
-        export function MotorStop(index: Motors): void {
-            MotorRun(index, 0);
-        }
+    // /**
+    //  * Execute two motors at the same time
+    //  * @param motor1 First Motor; eg: M1A, M1B
+    //  * @param speed1 [-255-255] speed of motor; eg: 150, -150
+    //  * @param motor2 Second Motor; eg: M2A, M2B
+    //  * @param speed2 [-255-255] speed of motor; eg: 150, -150
+    // */
+    // //% blockId=robotbit_motor_dual block="Motor|%motor1|speed %speed1|%motor2|speed %speed2"
+    // //% weight=84
+    // //% speed1.min=-255 speed1.max=255
+    // //% speed2.min=-255 speed2.max=255
+    // //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    // export function MotorRunDual(motor1: Motors, speed1: number, motor2: Motors, speed2: number): void {
+    //     MotorRun(motor1, speed1);
+    //     MotorRun(motor2, speed2);
+    // }
 
-        //% blockId=robotbit_stop_all block="Motor Stop All"
-        //% weight=79
-        //% blockGap=50
-        export function MotorStopAll(): void {
-            for (let idx = 1; idx <= 4; idx++) {
-                stopMotor(idx);
-            }
+    // /**
+    //  * Execute single motors with delay
+    //  * @param index Motor Index; eg: M1A, M1B, M2A, M2B
+    //  * @param speed [-255-255] speed of motor; eg: 150, -150
+    //  * @param delay seconde delay to stop; eg: 1
+    // */
+    // //% blockId=robotbit_motor_rundelay block="Motor|%index|speed %speed|delay %delay|s"
+    // //% weight=81
+    // //% speed.min=-255 speed.max=255
+    // //% name.fieldEditor="gridpicker" name.fieldOptions.columns=4
+    // export function MotorRunDelay(index: Motors, speed: number, delay: number): void {
+    //     MotorRun(index, speed);
+    //     basic.pause(delay * 1000);
+    //     MotorRun(index, 0);
+    // }
+
+
+
+    //% blockId=robotbit_stop block="Motor Stop|%index|"
+    //% weight=80
+    export function MotorStop(index: Motors): void {
+        MotorRun(index, 0);
+    }
+
+    //% blockId=robotbit_stop_all block="Motor Stop All"
+    //% weight=79
+    //% blockGap=50
+    export function MotorStopAll(): void {
+        for (let idx = 1; idx <= 4; idx++) {
+            stopMotor(idx);
         }
+    }
+
+    // //% blockId=robotbit_matrix_draw block="Matrix Draw|X %x|Y %y"
+    // //% weight=69
+    // export function MatrixDraw(x: number, y: number): void {
+    //     if (!initializedMatrix) {
+    //         matrixInit();
+    //         initializedMatrix = true;
+    //     }
+    //     let idx = y * 2 + x / 8;
+    //     matBuf[idx + 1] |= (1 << (x % 8));
+    //     matrixShow();
+    // }
+
+    // /*
+    // //% blockId=robotbit_matrix_clean block="Matrix Clean|X %x|Y %y"
+    // //% weight=68
+    // export function MatrixClean(x: number, y: number): void {
+    //     if (!initializedMatrix) {
+    //         matrixInit();
+    //         initializedMatrix = true;
+    //     }
+    //     let idx = y * 2 + x / 8;
+    //     // todo: bitwise not throw err 
+    //     matBuf[idx + 1] &=~(1 << (x % 8));
+    //     matrixShow();
+    // }
+    // */
+
+    // //% blockId=robotbit_matrix_clear block="Matrix Clear"
+    // //% weight=65
+    // //% blockGap=50
+    // export function MatrixClear(): void {
+    //     if (!initializedMatrix) {
+    //         matrixInit();
+    //         initializedMatrix = true;
+    //     }
+    //     for (let i = 0; i < 16; i++) {
+    //         matBuf[i + 1] = 0;
+    //     }
+    //     matrixShow();
+    // }
+
+    // //% blockId=robotbit_ultrasonic block="Ultrasonic|pin %pin"
+    // //% weight=10
+    // export function Ultrasonic(pin: DigitalPin): number {
+
+    //     // send pulse
+    //     pins.setPull(pin, PinPullMode.PullNone);
+    //     pins.digitalWritePin(pin, 0);
+    //     control.waitMicros(2);
+    //     pins.digitalWritePin(pin, 1);
+    //     control.waitMicros(10);
+    //     pins.digitalWritePin(pin, 0);
+
+    //     // read pulse
+    //     let d = pins.pulseIn(pin, PulseValue.High, 25000);
+    //     let ret = d;
+    //     // filter timeout spikes
+    //     if (ret == 0 && distanceBuf!= 0){
+    //         ret = distanceBuf;
+    //     }
+    //     distanceBuf = d;
+    //     return ret*10/6/58;
+    // }
+
 }
